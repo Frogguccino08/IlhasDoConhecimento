a9e1dd47cd3f1f7ab69b75e6dbbd5b31b31f528f
Frogguccino08 <raul.07@hotmail.com.br>
2025-08-14 18:15:17 -0300

    Segunda parte nova tela de personagem,
    Desenvolvendo uma tela diferente de personagem, mais dinamica

--- Assets/1Scripts/Nova tela perso/TelaSelecao.cs
+++ Assets/1Scripts/Nova tela perso/TelaSelecao.cs
@@ -10,25 +10,76 @@ public class TelaSelecao : MonoBehaviour
     public GameObject pequeno;
     public GameObject canva;
 
+    int down = 0;
+    string material = "???";
+
     void Start()
     {
         foreach (PCsSO boneco in perso)
         {
-            int index = perso.IndexOf(boneco);
-            int down = 0;
+            int o = 0;
+            int index = 0;
 
-            for (int i = 0; i <= index; i++)
+            while (o <= perso.IndexOf(boneco))
             {
-                if ((index + 1) % 3 == 0)
+                index++;
+                o++;
+
+                if (index > 3)
+                {
+                    index = 1;
+                }
+            }
+
+            for (int i = 0; i <= perso.IndexOf(boneco) - 1; i++)
+            {
+                if ((index - 1) % 3 == 0 && index != 0)
                 {
                     down++;
                 }
             }
 
+            Debug.Log(perso[perso.IndexOf(boneco)].nome + ", down: " + down + ", index: " + index);
+
             persosDesc.Add(Instantiate(pequeno, transform.position, Quaternion.identity));
 
             persosDesc[perso.IndexOf(boneco)].transform.SetParent(canva.transform);
-            persosDesc[perso.IndexOf(boneco)].transform.localPosition = new Vector3(0.5f, 0.5f, 0);
+            persosDesc[perso.IndexOf(boneco)].transform.localPosition = new Vector3(-9.2f + ((index - 1) * 7), 5 - (down * 1), 0);
+
+            persosDesc[perso.IndexOf(boneco)].GetComponent<Persopequeno>().nome.text = perso[perso.IndexOf(boneco)].nome;
+            if (perso[perso.IndexOf(boneco)].nome.Length > 18)
+            {
+                persosDesc[perso.IndexOf(boneco)].GetComponent<Persopequeno>().nome.fontSize = 0.35f;
+            }
+            else
+            {
+                persosDesc[perso.IndexOf(boneco)].GetComponent<Persopequeno>().nome.fontSize = 0.4f;
+            }
+
+            switch (perso[perso.IndexOf(boneco)].material)
+            {
+                case 1:
+                    material = "Papel";
+                    break;
+                case 2:
+                    material = "Plástico";
+                    break;
+                case 3:
+                    material = "Vidro";
+                    break;
+                case 4:
+                    material = "Metal";
+                    break;
+                case 5:
+                    material = "Orgânico";
+                    break;
+            }
+            persosDesc[perso.IndexOf(boneco)].GetComponent<Persopequeno>().materialTxt.text = material;
+
+            if (perso.IndexOf(boneco) == perso.Count - 1)
+            {
+                down = 0;
+            }
         }
     }
 }
