89409df56adff13f977433d8cb49b5694c64039f
Frogguccino08 <raul.07@hotmail.com.br>
2026-02-24 10:32:31 -0300

    Preparações para a aula

---
+++ Assets/Aula/Aula.cs
@@ -0,0 +1,42 @@
+/*
+Pre-game:
+    Visual code se não tiverem
+    GitHub desktop como funciona e clonar repositório
+    Unity e Editor, configurações do visual code
+
+Basicos:
+    váriaveis e os tipos (public, private)
+    comentários
+    Funções Start Update, função e Ienumerator
+    Debug.Log
+
+Objetos:
+    Adicionar demoninadores [hide e serialize]
+    Criar objeto em jogo
+    Adicionar script
+    chamar outros objetos em código e GetComponent
+
+Especificos:
+    Canva, e TMP_text
+
+Objetivo final:
+    Criar um texto na tela com um numero
+    Cada um criar um botão para fazer uma operação nesse número    
+*/
+
+using UnityEngine;
+
+public class Aula : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
