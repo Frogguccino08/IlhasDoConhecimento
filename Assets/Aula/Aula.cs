9602cbaf0f97097e09ba2478a970acb9896a7f51
Frogguccino08 <raul.07@hotmail.com.br>
2026-02-26 09:36:50 -0300

    Aula daquele jeito

--- Assets/Aula/Aula.cs
+++ Assets/Aula/Aula.cs
@@ -1,27 +1,50 @@
 /*
-Pre-game:
-    Visual code se não tiverem
-    GitHub desktop como funciona e clonar repositório
-    Unity e Editor, configurações do visual code
+Variaveis:
+    string
+    int
+    float (coloca um f no final de numeros quebrados como "1.4f")
+    bool
 
-Basicos:
-    váriaveis e os tipos (public, private)
-    comentários
-    Debug.Log
-    Funções Start Update, função e Ienumerator
-    
-Objetos:
-    Adicionar demoninadores [hide e serialize]
-    Criar objeto em jogo
-    Adicionar script
-    chamar outros objetos em código e GetComponent
+Tipos de variaveis e funções:
+    public (ver no inspetor e ser usada em outros scripts)
+    private (exclusivo para aquele script)
 
-Especificos:
-    Canva, e TMP_text
+Testes:
+    Debug.Log(""); (Para escrever no console).
+    // Comentar uma linha */    /* */  /*para comentar uma parte
 
-Objetivo final:
-    Criar um texto na tela com um numero
-    Cada um criar um botão para fazer uma operação nesse número    
+Funções:
+    public void Nome(int i){ }
+    primeiro se é publico ou privado, depois o tipo da função normalmente void ou Ienumerator, depois o nome, dentro do () váriaveis da funções se tiver, { } Código
+
+    void Start(){ } função que acontece ao iniciar
+    void Updae() { } função que acontece todo frame (60fps)
+
+Fields:
+    [HideInInspector] Faz variaveis publicas não aparecerem no inspetor
+    [SerializeField] Faz variaveis privadas aparecerem no inspetor
+
+    Colocado na linha de cima de váriaveis
+
+Chamar objetos:
+    public GameObject nome; variavel do objeto de jogo, pode trocar o "GameObject" por componentes para facilitar, principalmente em scripts
+    GetComponent<Nome do componente>() acha um componente dentro de um outro objeto
+    .nome encontra variaveis e funções dentro de um componente
+
+Objetos importantes:
+    Script: O arquivo de código que você conecta a um objeto
+    Scene: O arquivo que salva uma sala do jogo
+    Canva: Permite colocar imagens sem estarem ligadas a um objeto, usado para colocar textos. feito para criar UI
+    TMP_Text: Texto em tela dos jogos
+
+Partes da tela:
+    Scene: Tela do jogo onde você mexe componentes
+    Game: O jogo como ele será rodado
+    Hierarquia: Mostra todos os objetos em cena e quais objetos são filhos de quais objetos
+    Inspetor: Onde mostra todos os componentes do objeto que você selecionou
+
+Miscs:
+    ScriptableObject: Um tipo de arquivo criado pelo próprio dev
 */
 
 using UnityEngine;
